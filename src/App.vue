a012e89e038c74de4073ff630fce4356fd926076
FAST <krinickij.v.a@gmail.com>
2024-09-27 18:07:48 +0300

    feat: created base structure + aded npm packages
    сделана минимальная правильная внутреннаяя структура
    Установлены npm пакеты :
    axios
    sass
    sass-embedded
    sass-loader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<script setup>
-
-</script>
-
 <template>
-ssssss
+  <div class="header">
+    <h1>Приложение для просмотра постов с </h1>
+    <a href="https://jsonplaceholder.typicode.com/" target="_blank">jsonplaceholder.typicode.com</a>
+  </div>
 </template>
 
-<style scoped>
+<script>
 
+
+export default {
+
+};
+</script>
+<style lang="scss" >
+  .header{
+    display: flex;
+    flex-direction: row;
+  }
 </style>
+
