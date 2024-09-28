7f0ea18f62d7eac9388e2e2a20725a7abf0fac8a
FAST <krinickij.v.a@gmail.com>
2024-09-28 09:46:53 +0300

    feat: created postDetails component structure
    сдедана структура компонента детального просмотра поста

--- src/components/PostDetails.vue
+++ src/components/PostDetails.vue
@@ -4,8 +4,18 @@
       <div v-if="!id" class="nodata">
         <h1>Выберите пост для получения полной информации о нем</h1>
       </div>
-      <div class="">
-{{ id }}
+      <div v-if="id" class="post-wrapper">
+        <div class="post">
+          <h1>post title</h1>
+          <div class="post-data">
+            <span> postData</span>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea porro voluptatibus dolore? Illum veniam blanditiis incidunt quis sed eum nesciunt ducimus beatae, laborum accusamus magnam ab odit. Consequuntur numquam totam provident vitae quos animi enim dicta quae perferendis pariatur, sunt eligendi facilis ut nobis, sit aspernatur quaerat nihil consequatur in dolorem. Neque aspernatur error commodi ullam recusandae doloribus tempore sequi sunt in quasi ipsam necessitatibus accusamus, quae voluptate. Laborum, sapiente saepe rerum voluptatem repellendus sit autem ex eos aperiam quae molestiae esse facilis at amet nemo corrupti repellat. Fuga facilis tenetur facere fugiat quas quia voluptatum id, possimus dolor odio.</p>
+          </div>
+        </div>
+        <div class="postComments">
+          
+        </div>
+          {{ id }}
       </div>
 </template>
 <script>
@@ -37,4 +47,12 @@ export default{
 }
 </script>
 <style lang="scss" scoped>
+  .post-wrapper{
+    padding:20px 
+  }
+  .post-data{
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    padding-top: 20px;
+  }
 </style>
