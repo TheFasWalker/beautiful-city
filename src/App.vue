519951faa8f33e223da83c3edeee5c0f75f33c0a
krinickij <krinickij.v.a@gmail.com>
2024-09-27 22:00:57 -0400

    feat: redesign
    Переделан дизайн главной страницы и компонента превью поста

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,43 @@
-
-
 <script>
 
 import ErrorMessage from './components/ui/ErrorMessage.vue';
 import Loader from './components/ui/Loader.vue';
-import PostPreview from './components/PostPreview.vue' 
+import PostPreview from './components/PostPreview.vue'
 
 export default {
-data(){
-  return{
-    loading:false,
-    postData:[],
-    errorTextDescription:''
-  }
-},
-  components: {
-    loader:Loader,
-    ErrorMessage:ErrorMessage,
-    PostPreview:PostPreview
+  data() {
+    return {
+      loading: false,
+      postData: [],
+      errorTextDescription: '',
+      postDataLoading: false,
+      postErrorTextDescription: '',
+      postData:[]
+    }
   },
-  mounted(){
+  components: {
+    loader: Loader,
+    ErrorMessage: ErrorMessage,
+    PostPreview: PostPreview
+  },
+  mounted() {
     this.getPosts();
   },
-  methods:{
-    getPosts: async function(){
-      try{
+  methods: {
+    getPosts: async function () {
+      try {
         this.loading = true
         this.error = ''
         const response = await fetch('https://jsonplaceholder.typicode.com/posts/')
-      if (!response.ok) {
+        if (!response.ok) {
           throw new Error('Network response was not ok')
         }
         this.postData = await response.json()
         console.log(this.postData)
-    }catch(error){
-      this.errorTextDescription='Error fetching posts: ' + error.message
-      console.log('asdf')
-    } finally {
+      } catch (error) {
+        this.errorTextDescription = 'Error fetching posts: ' + error.message
+        console.log('asdf')
+      } finally {
         this.loading = false
         this.error = ''
       }
@@ -44,52 +45,71 @@ data(){
     }
   }
 }
-;
+  ;
 </script>
-  <style lang="scss" >
-    .header{
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      gap: 20px;
-      width: 100%;
-      padding: 10px 0;
-    }
-    .main{
-      width: 100%;
-      // height: 100%;
-      min-height: 250px;
-      max-height: 700px;
-      position: relative;
-      overflow-y: auto;
-    }
-    .post-wrapper{
-      display: flex;
-      flex-direction: column;
-      gap: 10px;
-      overflow-y: auto;
-      width: 100%;
-    }
-  </style>
+<style lang="scss">
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  width: 100%;
+  padding: 10px 0;
+}
+
+.main {
+  width: 100%;
+  min-height: 250px;
+  position: relative;
+
+  display: grid;
+  grid-template-columns: 400px 1fr;
+}
+
+.sidebar,
+.content {
+  height: 100%;
+  overflow-y: auto;
+  position: relative;
+}
+.sidebar{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.nodata{
+
+  width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
 <template>
+
   <div class="header">
     <h1>Приложение для просмотра постов с </h1>
     <a href="https://jsonplaceholder.typicode.com/" target="_blank">jsonplaceholder.typicode.com</a>
   </div>
   <div class="main">
-    <loader v-if="loading"/>
-    <error-message v-if="errorTextDescription"
-    :errorText="errorTextDescription"/>
-    <div class="post-wrapper">
+    <div class="sidebar">
+      <loader v-if="loading" />
+      <error-message v-if="errorTextDescription" :errorText="errorTextDescription" />
       <PostPreview
         v-for="post of postData"
         :title="post.title"
-        :description="post.body"
         :id="post.id"
       />
+    </div>
+    <div class="content">
+      <loader v-if="postDataLoading" />
+      <error-message v-if="postErrorTextDescription" :errorText="errorTextDescription" />
+      <div v-if="!postData" class="nodata">
+        <h1>Выберите пост для получения полной информации о нем</h1>
+      </div>
     </div>
 
 
   </div>
 </template>
-
--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -1,20 +1,14 @@
 <template>
-    <div class="post">
-        <h2 class="h2">{{ title }}</h2>
-        <div class="description">
-            <span>post description</span>
-            <p>{{ description }}</p>
-            <button class="post-button" :data-id="id" >showComments</button>
-        </div>
-        
+    <div class="post" :data-id="id">
+        <span class="h2">{{ title }}</span>
+
+
     </div>
 </template>
 <script>
 export default{
     props: {
         title:String,
-        description:String,
-        id:Number
     }
 }
 </script>
@@ -23,13 +17,13 @@ export default{
         border: 2px solid rgb(153, 146, 146);
         border-radius: 10px;
         padding: 10px 20px;
-    }
-    .description{
-        padding-top: 20px;
-    }
-    .post-button{
-        margin-top: 20px;
         cursor: pointer;
+        &:hover{
+            background: gray;
+            box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+            color: #FFFFFF;
+        }
     }
-    
+
+
 </style>
