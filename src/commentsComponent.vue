a7ef2c7bd0269a19069ffebc9bcf4b59d49e033a
krinickij <krinickij.v.a@gmail.com>
2024-09-29 17:41:52 -0400

    feat: aded output for userData

--- src/commentsComponent.vue
+++ src/commentsComponent.vue
@@ -1,8 +1,79 @@
 <template>
-<div class="">second component</div>
+<div class="users">
+    <loader v-if="loading"/>
+    <error v-if="error" :errorText="error" />
+    <div class="header">
+        <h1>Приложение для просмотра пользователей с </h1>
+    <a href="https://jsonplaceholder.typicode.com/" target="_blank">jsonplaceholder.typicode.com</a>
+  </div>
+
+    <div class="users-data">
+    <userPreview v-for="user of usersData"
+    key="user.id"
+    :userData="user"/>
+</div>
+</div>
 </template>
 
 <script>
+import Loader from './components/ui/Loader.vue'
+import ErrorMessage from './components/ui/ErrorMessage.vue';
+import userPreview from './components/userPreview.vue'
+
+
+export default {
+    data() {
+        return {
+            usersData: [],
+            loading: false,
+            error:''
+        }
+    },
+    mounted() {
+        this.getUsersData()
+    },
+    components: {
+        loader: Loader,
+        error: ErrorMessage,
+        userPreview:userPreview
+    },
+
+    methods: {
+        getUsersData: async function () {
+            try {
+                this.loading = true
+                this.error = ''
+                const response = await fetch('https://jsonplaceholder.typicode.com/users')
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                this.usersData = await response.json()
+            } catch (error) {
+                this.error= 'Error fetching users: ' + error.message
+            } finally {
+                this.loading = false
+                this.error=''
+            }
+            console.log(this.usersData)
+        }
+    }
+}
 </script>
 <style lang="scss" scoped>
+.users{
+    position: relative
+}
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  width: 100%;
+  padding: 10px 0;
+}
+.users-data{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
 </style>
--- src/components/userPreview.vue
+++ src/components/userPreview.vue
@@ -1,6 +1,85 @@
 <template>
+    <div class="user-wrapper">
+        <div class="user-item">
+            <span>UserName: </span>
+            <span>{{ userData.name }}</span>
+        </div>
+        <div class="user-item">
+            <span>userEmail: </span>
+            <a href="mailto:{{ userData.email }}">{{ userData.email }}</a>
 
+        </div>
+        <div class="user-item">
+            <span>userCompany: </span>
+            {{ userData.company.name }}
+        </div>
+        <button @click="showData = !showData">Показать дополнительные данные</button>
+        <div class="user-hidden" v-show="showData">
+            <div class="user-elem">
+                <div class="user-item">
+                    <span>city:</span>
+                    <span>{{ userData.address.city }}</span>
+                </div>
+                <div class="user-item">
+                    <span>suite:</span>
+                    <span>{{ userData.address.suite }}</span>
+                </div>
+                <div class="user-item">
+                    <span>zipcode:</span>
+                    <span>{{ userData.address.city }}</span>
+                </div>
+                <div class="user-item">
+                    <span>city:</span>
+                    <span>{{ userData.address.zipcode }}</span>
+                </div>
+            </div>
+            <div class="user-elem">
+                <div class="user-item">
+                    <span>UserPhone:</span>
+                    <a href="tel:{{ userData.phone }}">{{ userData.phone }}</a>
+                </div>
+
+                <div class="user-item">
+                    <span>UserWebsite:</span>
+                    <a href="tel:{{ userData.website }}">{{ userData.website }}</a>
+                </div>
+            </div>
+        </div>
+
+</div>
 </template>
 <script>
+export default {
+    data() {
+        return {
+            showData:false
+        }
+    },
+    props: {
+        userData:Array
+    }
+}
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.user-item {
+
+    span:first-child{
+        font-weight: 700;
+    }
+}
+.user-wrapper{
+    box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+    padding: 20px 10px;
+}
+    .user-hidden{
+        display: grid;
+        grid-template-columns:  1fr 1fr 1fr 1fr;
+    }
+    .user-elem{
+        display: flex;
+        flex-direction: column;
+        padding-top: 10px;
+    }
+
+
+</style>
