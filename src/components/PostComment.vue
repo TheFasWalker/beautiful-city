31adf72dcf2f3f3bf0274ae4d99ba39502be0c57
krinickij <krinickij.v.a@gmail.com>
2024-09-29 11:16:15 -0400

    feat: aded userdata popup
    Написана функция запроса данных пользователя оставившего комментарий
    Добавлен попап для вывода данных пользователя

--- src/components/PostComment.vue
+++ src/components/PostComment.vue
@@ -2,18 +2,52 @@
     <div class="comment">
         <div class="header">
             <span class="username"> {{ username }}</span>
-            <button :data-userid='email'>UserData</button>
+            <button @click="getUserData">UserData</button>
         </div>
 
         <p>{{ body }}</p>
+        <div class="userData" v-if="popupState">
+            <button @click="closeUserPopup">close</button>
+            <div class="userData-item">
+                <span>username:</span>
+                <span>UserName</span>
+            </div>
+            <div class="userData-item">
+                <span>useremail:</span>
+                <span>useremail@email.email</span>
+            </div>
+            <div class="userData-item">
+                <span>companyName:</span>
+                <span>useremail@email.email</span>
+            </div>
+            <div class="userData-item">
+                <span>UserWebsite:</span>
+                <span>useremail@email.email</span>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            popupState:false
+    }
+},
     props: {
         username: String,
         body: String.appl,
-        email:String
+        email: String,
+    },
+    methods: {
+        getUserData: function () {
+            console.log(this.email)
+            this.popupState = !this.popupState
+
+        },
+        closeUserPopup: function () {
+            this.popupState = !this.popupState
+        }
     }
 }
 </script>
@@ -22,6 +56,7 @@ export default {
         border: 2px solid rgb(34, 185, 34);
         border-radius: 10px;
         padding: 20px 30px;
+        position: relative;
     }
     .username{
         font-size: 25px;
@@ -31,6 +66,23 @@ export default {
         display: flex ;
         flex-direction: row;
         justify-content: space-between;
+    }
+    .userData{
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 20px 10px ;
+        background-color: #ffffff;
+        box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+        border-radius: 10px;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        span:first-child{
+            font-size: 18px;
+            font-weight: 600;
+        }
     }
 
 </style>
